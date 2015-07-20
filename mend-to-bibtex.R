950d665312dac5c667bfbecabe2cad16080385c0
Ramon Diaz-Uriarte (at Coleonyx) <rdiaz02@gmail.com>
2015-07-20 17:32:58 +0200

    added some documentation/comments to main file

--- mend-to-bibtex.R
+++ mend-to-bibtex.R
@@ -1,24 +1,62 @@
+######################################################################
+######################################################################
+####                                                              ####
+####        How this thing works                                  ####
+####                                                              ####
+######################################################################
+######################################################################
+
+
+## Input:
+##    - MendeleySQL: the mendeley sqlite db (make a copy just in case)
+##    - BibTexFile: the bibtex file that Mendeley produces.
+
+## Before starting, remember to clean the trash of mendeley and then copy
+## or link the sqlite file.
+
+
+## Output
+##   - out: a bibtex file that you can then import into Zotero
+
+## Other things you need to specify and we assume:
+
+##   - rootFileDir: we assume all your PDFs (or ps, or tar.gz ---any file
+##   associated with an entry) live in directories as follows:
+##         /rootFileDir/some_other_directory/file_name.pdf
+
+##   where the some_other_directory is generally the name of the entry or
+##   similar.  The name is not very relevant. What matters here is that I
+##   assume that there is exactly one directory between the name of the
+##   file and the rootFileDir. This of course need not be so. It is the
+##   way I have things organized, and the code expects that. Change at
+##   will. (Search for functions that use 'rootFileDir')
+
+##   - tmpFilePaths: a temporary holder for files. We rename many
+##   files. This directory needs to exist. Files will be placed in there,
+##   and Zotero will take files from there. After all is done, you can
+##   remove it.
+
+
+
+
+## Remember to empty the trash of mendeley
 MendeleySQL <- "mend.sqlite"
 BibTeXFile <- "library-fixed.bib"
-out <- "new-library.bib"
-rootFileDir <- "/home/ramon/Mendeley-pdfs"
-tmpFilePaths <- "/home/ramon/tmp/mend"
-
+out <- "new-library.bib" ## the new bibtex file that will be created
+rootFileDir <- "/home/ramon/Mendeley-pdfs" ## The Mendeley pdfs hang from here.
+tmpFilePaths <- "/home/ramon/tmp/mend" ## A temporary directory for
+                                       ## placing renamed files.
 
 source("sqlite-bibtex-functions.R")
 
-## Remember to empty the trash of mendeley
+
 con <- dbConnect(SQLite(), MendeleySQL)
 minimalDBchecks(con)
-
 ## Continue if things are ok
-
 
 res <- dbGetQuery(con, sqliteQuery1) 
 res$timestamp <- getTimestamp(res)
 minimalDBDFchecks(res)
-
-
 
 
 bibfile <- myBibtexReaderandCheck(BibTeXFile)
@@ -43,72 +81,10 @@ jabrefGr <- jabrefGroups(con, res)
 
 outFullBibTex(bibfileFileFixed, jabrefGr, out)
 
-## - write the script to clean bibtex for tablet: entr project: http://entrproject.org/
-## - hel-bibtex issues?
 
-## df1 <- dbReadTable(con, "DocumentFolders")[, -3] ## remove "status" column
-## df2 <- dbReadTable(con, "DocumentFoldersBase")
-## folders <- rbind(df1, df2)
-
-## folders <- foldersDBread(con)
-## Then use bibtex key and folder name.
-## folderDocuments <- by(folders, folders$folderId,
-##                       function(x) {unique(x$documentId)})
-
-## folderNames <- dbReadTable(con, "Folders")[, c(1, 3, 4)]
-
-
-
-## folderNames$depth <- 0
-## folderNames$depth[folderNames$parentId %in% c(0, -1) ] <- 1
-## depthFolder <- function(id, df = folderNames) {
-##     ## In terms of id, because easier for error checking.
-##     pos <- which(df$id == id)
-##     parentId <- df[pos, "parentId"]
-##     if(parentId %in% c(0, -1) ) return(1)
-##     else {
-##         posParent <- which(df$id == parentId)
-##         return(df[posParent, "depth"] + 1)
-##     }
-## }
-## changesDepth <- TRUE
-## while(changesDepth) {
-##     formerDepth <- folderNames$depth
-##     folderNames$depth <- sapply(folderNames$id, depthFolder)
-##     if(all(formerDepth == folderNames$depth))
-##         changesDepth <- FALSE
-## }
-
-
-
-
-## folderNames$depth <- computeFolderDepth(folderNames)
-## orderedFolderNames <- orderFolderNames(folderNames)
-
-
-
-
-
-## to do still:
-
-## rename long file names
-## add info to bibtex
-## check bibtex errors/warnings
-
-
-## bibtex0 <- readLines(con = "mini.bib")
-
-
-
-## bibfile <- myBibtexReader(bibtex0)
-## bibtex2 <- readLines(con = "library.bib")
-## bibfile2 <- myBibtexReader(bibtex2)
-
-## why do I have NAs in bibtex keys? Because many things are not exported
-## to bibtex! Any that do not have keys!!!
-
-## FIXME: check num rows in dd and length bibfile are the same!
-## add mendeley id to bibtex too
+## You should have the bibtex file in the one you called out.  Go import
+## that into Zotero. You might want to first import into JabRef and see
+## what happens.
 
 
 
@@ -116,81 +92,9 @@ outFullBibTex(bibfileFileFixed, jabrefGr, out)
 
 
 
-## Miscell stuff
-## Notes in the PDF
-dbReadTable(con, "FileNotes")[1:10, ] ## notes fields note and baseNote
-## are identical But those I already have in the PDF. Since easy and
-## cheap, make sure I have all.
-
-## Notes in the entry itself
-cucu <- dbReadTable(con, "DocumentNotes")
-## use the text field
-
-## I need date added
-
-dbListFields(con, "Documents")
-
-
-
-
-dd <- dbReadTable(con, "Documents")
-
-res7 <- dbGetQuery(con, "
-SELECT
-Documents.id AS Ref_id,
-Documents.citationKey AS Ref_BibtexKey,
-cast(Documents.added as real) AS Ref_added,
-DocumentNotes.text AS Ref_notes
-FROM
-Documents
-LEFT OUTER JOIN DocumentNotes ON Documents.id = DocumentNotes.documentId 
-")
-res7$timestamp <- as.character(format(round(as.POSIXct("1970-01-01",
-                                                       tz="GMT+2") +
-                                                           res7$Ref_added/1000,
-                                            "secs")))
-dim(res7); names(res7)
-
-
-as.character(format(round(as.POSIXct("1970-01-01", tz="GMT+2") + res7$Ref_added/1000,  "secs")))[1]
 
 
 
 
 
-## all the tables
-tables <- dbListTables(con)
-sapply(tables, function(x) dbListFields(con, x))
 
-
-## - exporting from Mendeley the folders
-##     - It is in the DocumentFolders and DocumentFoldersBase (for names)
-
-
-
-## check the two books for hpbbm
-
-
-
-
-## resPdf <- dbGetQuery(con, "
-## SELECT FileNotes.documentId, group_concat(FileNotes.note, ' \n ')
-## FROM FileNotes
-## GROUP BY FileNotes.documentId
-## ")
-
-
-
-## res <- dbGetQuery(con, "
-## SELECT
-## Documents.id AS Ref_id,
-## Documents.citationKey AS Ref_BibtexKey,
-## cast(Documents.added as real) AS Ref_added,
-## DocumentNotes.text AS Ref_notes,
-## (SELECT group_concat(FileNotes.note, ' \n ')
-##  FROM FileNotes GROUP BY FileNotes.documentId
-##  ) AS Ref_PDFnotes
-## FROM
-## Documents
-## LEFT OUTER JOIN DocumentNotes ON Documents.id = DocumentNotes.documentId
-## LEFT OUTER JOIN FileNotes ON Documents.id = FileNotes.documentId")
