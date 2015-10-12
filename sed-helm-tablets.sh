1d97c0b80c806be8c90f981a66449244efd72478
ramon diaz-uriarte (at Bufo) <rdiaz02@gmail.com>
2015-10-12 16:37:31 +0200

    well, no, I need BBTs collections in tablet

--- sed-helm-tablets.sh
+++ sed-helm-tablets.sh
@@ -9,64 +9,86 @@
 ## Thus, I use zotero auto export (http://rokdd.de/b/zotero-autoexport) to
 ## export the plain bib every hour.
 
-
-OUT=$1
+## Well, no, because I do not want to loose the group structure for the
+## tablet. Keep the two below, but back to single case.
 
 ## Or pass the dir as argument, or whatever
 ZOTDIR="/home/ramon/Zotero-data/storage"
 
 
-if [[ $OUT == "helm" ]]; then
-    sed -e '/file = {/{
+
+sed -e '/file = {/{
     s/{[^/:]*:/{ /
     s/:[^/:}]*\/[^/:}]*}/ }/
     s/:[^/:]*\/[^/:]*;/ /g
     s/ [^/:]*:/ /g
 }' $ZOTDIR/zotero-$HOSTNAME.bib > $ZOTDIR/helm.bib
 
-    ## For helm we do not need the jabref group structure
-    sed -i -n '/@comment{jabref-meta: groupsversion:3;}/q;p' $ZOTDIR/helm.bib
-fi
-
-if [[ $OUT == "tablet" ]]; then
-
-    ##  Erathostenes had a bug and crashed with groups. Is it fixed now? We
-    ##  keep them for now. Note that Erathostenes can use paths without the
-    ##  exact jabref format. For info about paths see this issue:
-    ##  https://bitbucket.org/mkmatlock/eratosthenes/issues/206/attachment-location
-    ##  so it seems we want to remove all the path, except what hangs from the common
-    ##  location of bib and files.
-
-    # sed -e '/file = {/{
-    #      s/\/home\/ramon\/Zotero-data\/storage//g 
-    # }' $ZOTDIR/helm.bib > $ZOTDIR/tablet-erathostenes.bib
-
-
-    # sed -e '/file = {/{
-    #     s/\/home\/ramon\/Zotero-data\/storage/\/sdcard\/Zotero-storage/g 
-    # }' $ZOTDIR/helm.bib > $ZOTDIR/tablet-erathostenes.bib
-
-    # This is a larger file. Not sure I want all the extra stuff.
     sed -e '/file = {/{
     s/\/home\/ramon\/Zotero-data\/storage//g 
-}' $ZOTDIR/zotero-$HOSTNAME-plain-bib.bib > $ZOTDIR/tablet-erathostenes.bib
-
-
-
+}' $ZOTDIR/zotero-$HOSTNAME.bib > $ZOTDIR/tablet-erathostenes.bib
 
     ## In the tablet, Library uses the same file as Refmaster. Library can
     ## deal with multiple attached files per entry. With RefMaster you can
     ## only use one file. It uses relative paths
     sed -e '/file = {/{
     s/\/home\/ramon\/Zotero-data\/storage//g 
-}' $ZOTDIR/zotero-$HOSTNAME-plain-bib.bib > $ZOTDIR/tablet-refmaster.bib
+}' $ZOTDIR/zotero-$HOSTNAME.bib > $ZOTDIR/tablet-refmaster.bib
     ## And remove jabref groups
     sed -i -n '/@comment{jabref-meta: groupsversion:3;}/q;p' $ZOTDIR/tablet-refmaster.bib
 
-fi
+
+
+# OUT=$1
+# if [[ $OUT == "helm" ]]; then
+#     sed -e '/file = {/{
+#     s/{[^/:]*:/{ /
+#     s/:[^/:}]*\/[^/:}]*}/ }/
+#     s/:[^/:]*\/[^/:]*;/ /g
+#     s/ [^/:]*:/ /g
+# }' $ZOTDIR/zotero-$HOSTNAME.bib > $ZOTDIR/helm.bib
+
+#     ## For helm we do not need the jabref group structure
+#     sed -i -n '/@comment{jabref-meta: groupsversion:3;}/q;p' $ZOTDIR/helm.bib
+# fi
+
+# if [[ $OUT == "tablet" ]]; then
+
+#     ##  Erathostenes had a bug and crashed with groups. Is it fixed now? We
+#     ##  keep them for now. Note that Erathostenes can use paths without the
+#     ##  exact jabref format. For info about paths see this issue:
+#     ##  https://bitbucket.org/mkmatlock/eratosthenes/issues/206/attachment-location
+#     ##  so it seems we want to remove all the path, except what hangs from the common
+#     ##  location of bib and files.
+
+#     # sed -e '/file = {/{
+#     #      s/\/home\/ramon\/Zotero-data\/storage//g 
+#     # }' $ZOTDIR/helm.bib > $ZOTDIR/tablet-erathostenes.bib
+
+
+#     # sed -e '/file = {/{
+#     #     s/\/home\/ramon\/Zotero-data\/storage/\/sdcard\/Zotero-storage/g 
+#     # }' $ZOTDIR/helm.bib > $ZOTDIR/tablet-erathostenes.bib
+
+#     # This is a larger file. Not sure I want all the extra stuff.
+#     sed -e '/file = {/{
+#     s/\/home\/ramon\/Zotero-data\/storage//g 
+# }' $ZOTDIR/zotero-$HOSTNAME-plain-bib.bib > $ZOTDIR/tablet-erathostenes.bib
+
+#     ## In the tablet, Library uses the same file as Refmaster. Library can
+#     ## deal with multiple attached files per entry. With RefMaster you can
+#     ## only use one file. It uses relative paths
+#     sed -e '/file = {/{
+#     s/\/home\/ramon\/Zotero-data\/storage//g 
+# }' $ZOTDIR/zotero-$HOSTNAME-plain-bib.bib > $ZOTDIR/tablet-refmaster.bib
+#     ## And remove jabref groups
+#     sed -i -n '/@comment{jabref-meta: groupsversion:3;}/q;p' $ZOTDIR/tablet-refmaster.bib
+
+# fi
 
 
 
-## Now, for using entr and automatic processing use something like this in .xsession or similar:
-## ls ~/Zotero-data/storage/zotero-$HOSTNAME.bib | entr ~/Adios_Mendeley/sed-helm-tablets.sh helm &
-## ls ~/Zotero-data/storage/zotero-$HOSTNAME-plain-bib.bib | entr ~/Adios_Mendeley/sed-helm-tablets.sh tablet &
+# ## Now, for using entr and automatic processing use something like this in .xsession or similar:
+# ## ls ~/Zotero-data/storage/zotero-$HOSTNAME.bib | entr ~/Adios_Mendeley/sed-helm-tablets.sh helm &
+# ## ls ~/Zotero-data/storage/zotero-$HOSTNAME-plain-bib.bib | entr ~/Adios_Mendeley/sed-helm-tablets.sh tablet &
+
